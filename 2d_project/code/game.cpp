47ecd2c467146650d859f9f03834e1944e3fd08a
Olle <ollenassen1992@gmail.com>
2019-09-04 13:17:56 +0200

    borken shit 2.0

--- 2d_project/code/game.cpp
+++ 2d_project/code/game.cpp
@@ -1,9 +1,21 @@
 #include "game.h"
 #include "shader.h"
 #include "glad/glad.h"
+#include "math2d.h"
+
+struct Quad
+{
+	Vector2 abcd[4] = 
+	{
+		vector2(-0.5f, -0.5f),
+		vector2(0.5f, 0.5f),
+		vector2(-0.5f, 0.5f),
+		vector2(0.5f, 0.5f)
+	};
+};
 
 #define PI 3.14159
-#include <iostream>
+
 void game_initialize(Game & game)
 {
 	game.tilemap.size_x = 1280 / 16;
@@ -13,16 +25,20 @@ void game_initialize(Game & game)
 	glBindVertexArray(game.global_vao);
 	glGenBuffers(1, &game.tilemap.vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, game.tilemap.vbo);
-	glm::vec2 vertexData[16][16];
+	Quad vertexData[16][16];
 	for (int i = 0; i < 16; ++i)
 	{
 		for (int j = 0; j < 16; ++j)
 		{
 			game.tilemap.xy[j][i] = '-1';
-			vertexData[j][i] = glm::vec2(j * game.tilemap.size_x, i * game.tilemap.size_y);
+			vertexData[j][i].abcd[0] = vector2_add(vertexData[j][i].abcd[0], vector2(j * game.tilemap.size_x, i * game.tilemap.size_y));
+			vertexData[j][i].abcd[1] = vector2_add(vertexData[j][i].abcd[1], vector2(j * game.tilemap.size_x, i * game.tilemap.size_y));
+			vertexData[j][i].abcd[2] = vector2_add(vertexData[j][i].abcd[2], vector2(j * game.tilemap.size_x, i * game.tilemap.size_y));
+			vertexData[j][i].abcd[3] = vector2_add(vertexData[j][i].abcd[3], vector2(j * game.tilemap.size_x, i * game.tilemap.size_y));
 		}
 	}
-	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 16 * 16, &vertexData[0], GL_STATIC_DRAW);
+	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Quad) * 16 * 16, &vertexData[0], GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
 
@@ -47,5 +63,5 @@ void game_render(Game& game)
 	shaderUniform(game.shader, "projection", game.camera.ortho);
 	glBindBuffer(GL_ARRAY_BUFFER, game.tilemap.vbo);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
-	glDrawArrays(GL_POINTS, 0, 16 * 16);
+	glDrawArrays(GL_TRIANGLES, 0, 16 * 16);
 }
