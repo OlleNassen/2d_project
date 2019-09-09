cc07d05612128ab8a4a498977721d6a266a77933
deadvard <edvardleiborn@telia.com>
2019-09-09 12:33:10 +0200

    half camera

--- 2d_project/code/camera.cpp
+++ 2d_project/code/camera.cpp
@@ -1,8 +1,75 @@
 #include "camera.h"
-#include "math2d.h"
+#include "window.h"
 
 void camera_initialize_default(Camera& camera)
 {
 	camera.ortho = mat4_create_ortho(0.0f, 1280.f, 0.0f, 720.f, -1.f, 1.f);
 	camera.position = vector3_create(0,0,1);
+}
+
+Vector2 camera_update(Camera *camera)
+{
+	camera->state = mov_none;
+	int mx = camera->mx;
+	int my = camera->my;
+	window_mouse_position(&mx, &my);
+
+	if (mx != camera->mx || my != camera->my)
+	{
+		camera->state = mov_mouse_cursor;
+	}
+
+	GameButton up;
+	GameButton down;
+	GameButton left;
+	GameButton right;
+
+	if (camera->state == mov_none)
+	{
+		/*up = window_keyboard_pressed(camera->keyboard_keys[0]);
+		down = window_keyboard_pressed(camera->keyboard_keys[1]);
+		left = window_keyboard_pressed(camera->keyboard_keys[2]);
+		right = window_keyboard_pressed(camera->keyboard_keys[3]);*/
+		if (up.pressed || down.pressed || left.pressed || right.pressed)
+		{
+			camera->state = mov_mouse_buttons;
+		}			
+	}
+	
+	if (camera->state == mov_none)
+	{
+		up = window_keyboard_pressed(camera->keyboard_keys[0]);
+		down = window_keyboard_pressed(camera->keyboard_keys[1]);
+		left = window_keyboard_pressed(camera->keyboard_keys[2]);
+		right = window_keyboard_pressed(camera->keyboard_keys[3]);
+		if (up.pressed || down.pressed || left.pressed || right.pressed)
+		{
+			camera->state = mov_keyboard_keys;
+		}
+	}
+
+	if (camera->state == mov_none)
+	{
+		up = window_keyboard_pressed(camera->keyboard_keys[0]);
+		down = window_keyboard_pressed(camera->keyboard_keys[1]);
+		left = window_keyboard_pressed(camera->keyboard_keys[2]);
+		right = window_keyboard_pressed(camera->keyboard_keys[3]);
+		if (up.pressed || down.pressed || left.pressed || right.pressed)
+		{
+			camera->state = mov_gamepad_buttons;
+		}
+	}
+
+	if (camera->state != mov_mouse_cursor)
+	{
+		Vector2 direction = vector2_create(0.0f, 0.0f);
+		if (up.pressed) direction.y += 1.0f;
+		if (down.pressed) direction.y -= 1.0f;
+		if (left.pressed) direction.y -= 1.0f;
+		if (right.pressed) direction.y += 1.0f;
+		
+		camera->position.x += direction.x * camera->speed;
+		camera->position.y += direction.y * camera->speed;
+	}
+	
 }
--- 2d_project/code/camera.h
+++ 2d_project/code/camera.h
@@ -1,13 +1,35 @@
 #ifndef CAMERA_H
 #define CAMERA_H
-#include <math2d.h>
+#include "math2d.h"
+
+typedef enum
+{
+	mov_none,
+	mov_mouse_cursor,
+	mov_mouse_buttons,
+	mov_keyboard_keys,
+	mov_gamepad_buttons
+} CameraState;
 
 struct Camera
 {
 	Mat4 ortho;
 	Vector3 position;
+
+	float speed;
+
+	int mx;
+	int my;
+
+	unsigned int mouse_buttons[4];
+	unsigned int keyboard_keys[4];
+	unsigned int gamepad_buttons[4];
+
+	CameraState state;
 };
 
 void camera_initialize_default(Camera& camera);
+
+Vector2 camera_update(Camera *camera);
 
 #endif
