7eda9c33eddc0115625cc0deb5d4184de8dcdc93
Deadvard <edvardleiborn@telia.com>
2019-09-26 14:50:48 +0200

    fix

--- 2d_project/code/drawer.cpp
+++ 2d_project/code/drawer.cpp
@@ -93,9 +93,9 @@ void generate_tilemap(Drawer& drawer, const Uint32* type_data, unsigned short he
 			drawer.vertex_local_coords[i + j * width][2] = rectangle_coordinates[2];
 			drawer.vertex_local_coords[i + j * width][3] = rectangle_coordinates[3];
 
-			drawer.vertex_colors[i + j * width].r = '255';
-			drawer.vertex_colors[i + j * width].g = '255';
-			drawer.vertex_colors[i + j * width].b = '255';
+			drawer.vertex_colors[i + j * width].r = 255;
+			drawer.vertex_colors[i + j * width].g = 255;
+			drawer.vertex_colors[i + j * width].b = 255;
 
 			int tile_number = type_data[i + j * width] + 32*15; // 32*15 is to offset to the tiles in the one texture
 
@@ -150,9 +150,9 @@ void generate_actors(Drawer& drawer, int num_actors, unsigned short classes[])
 		drawer.vertex_local_coords[drawer.total_num_vertices/4][2] = rectangle_coordinates[2];
 		drawer.vertex_local_coords[drawer.total_num_vertices/4][3] = rectangle_coordinates[3];
 
-		drawer.vertex_colors[drawer.total_num_vertices/4].r = '255';
-		drawer.vertex_colors[drawer.total_num_vertices/4].g = '0';
-		drawer.vertex_colors[drawer.total_num_vertices/4].b = '0';
+		drawer.vertex_colors[drawer.total_num_vertices/4].r = 255;
+		drawer.vertex_colors[drawer.total_num_vertices/4].g = 0;
+		drawer.vertex_colors[drawer.total_num_vertices/4].b = 0;
 	
 		int tile_number = classes[i] * 4;
 	
